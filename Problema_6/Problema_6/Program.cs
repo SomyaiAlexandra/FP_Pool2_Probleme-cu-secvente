0b6572eb43e4c64e4a05d41a5af163445f6a1fbd
Alexandra Somyai <somyai.alexandra@yahoo.com>
2021-01-20 17:13:11 +0200

    Finish code.

---
+++ Problema_6/Problema_6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_6
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar: ");
+            int n;
+
+            try
+            {
+                Console.Write("n: ");
+                if (!int.TryParse(Console.ReadLine(), out n))
+                    throw new Exception("Nu ati introdus un numar !");
+
+                bool ok = true;
+                int x = rnd.Next(31);
+
+                Console.WriteLine("Secventa data este: ");
+                Console.Write($"{x} ");
+                for (int i = 0; i < n; i++)
+                {
+                    int y = rnd.Next(31);
+                    Console.Write($"{y} ");
+
+                    if (!(x <= y))
+                        ok = false;
+
+                    x = y;
+
+                }
+
+                if (ok)
+                    Console.WriteLine("Secventa data are numerele in oridne CRESCATOARE.");
+                else
+                    Console.WriteLine("Secventa data NU are numerele in oridne CRESCATOARE.");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
