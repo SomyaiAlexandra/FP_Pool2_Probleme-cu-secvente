972248d1aab6c92fb0df795f28b85d53ce7aea54
Alexandra Somyai <somyai.alexandra@yahoo.com>
2021-01-20 17:18:00 +0200

    Finish code.

---
+++ Problema_12/Problema_12/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_12
+{
+    class Program
+    {
+        // Cate grupuri de numere consecutive diferite de zero sunt intr-o secventa de n numere. 
+        // Considerati fiecare astfel de grup ca fiind un cuvant, zero fiind delimitator de cuvinte. 
+        // De ex. pentru secventa 1, 2, 0, 3, 4, 5, 0, 0, 6, 7, 0, 0 raspunsul este 3.
+
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti un numar: ");
+            int n;
+
+            try
+            {
+                Console.Write("n: ");
+                if (!int.TryParse(Console.ReadLine(), out n))
+                    throw new Exception("Nu ati introdus un numar !");
+
+                int nr = 1;
+                double x;
+
+
+                Console.WriteLine("Secventa este: ");
+                for (int i = 0; i < n; i++)
+                {
+                    x = rnd.Next(11);
+
+                    if (x != 0)
+                        Console.Write($"{x} ");
+                    else
+                    {
+                        Console.WriteLine();
+                        nr++;
+                    }
+
+                }
+                Console.WriteLine();
+                Console.WriteLine($"In secventa data sunt {nr} secvente cu elemente nenule.");
+                Console.WriteLine();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
